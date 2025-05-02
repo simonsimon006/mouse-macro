f0320ea3e751e82b0d17a904cb1a153e1bde5fc7
Simon Grunwald <simongrunwald@hotmail.de>
2025-05-02 13:05:33 +0200

    cha: properly adds trouble crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use trouble_host::prelude::*;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
